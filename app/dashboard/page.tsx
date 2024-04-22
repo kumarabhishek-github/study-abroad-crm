26ed0f0dcf08f40181192f6a9e1d87492931e0f9
kumarabhishek-github <eyemabhishek@gmail.com>
2024-04-22 12:35:33 +0530

    demo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,4 @@
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import TaskManagementTabble from '@/app/ui/dashboard/taskmanagement';
 import { Button } from '../ui/button';  
 export default async function Page() {
